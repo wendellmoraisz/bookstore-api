854d0e57ffc2aebbce4655bc9f2d6c059412b6a1
Wendell <wendellmorais3319@gmail.com>
2022-08-09 10:24:20 -0300

    feat: cadastrar livro

--- src/main/java/com/bookstore/api/controllers/BookController.java
+++ src/main/java/com/bookstore/api/controllers/BookController.java
@@ -3,8 +3,12 @@ package com.bookstore.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.api.models.Book;
@@ -20,6 +24,12 @@ public class BookController {
     @GetMapping
     public List<Book> getBooks() {
         return bookRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book createBook(@RequestBody Book book){
+        return bookRepository.save(book);
     }
 
 }
