ff77a724039ece37e3e273f11269c9439c2f8613
Wendell <wendellmorais3319@gmail.com>
2022-08-10 10:14:05 -0300

    feat: cadastrar cliente

--- src/main/java/com/bookstore/api/controllers/ClientController.java
+++ src/main/java/com/bookstore/api/controllers/ClientController.java
@@ -3,12 +3,17 @@ package com.bookstore.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.api.models.Client;
 import com.bookstore.api.repository.ClientRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/clientes")
@@ -22,4 +27,10 @@ public class ClientController {
         return clientRepository.findAll();
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client postMethodName(@RequestBody Client client) {        
+        return clientRepository.save(client);
+    }
+    
 }
