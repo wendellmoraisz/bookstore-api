547f84d6ab22eac3ee1532d2185e0eb5894a31bb
Wendell <wendellmorais3319@gmail.com>
2022-08-10 09:56:45 -0300

    feat: criar venda

--- src/main/java/com/bookstore/api/controllers/SaleController.java
+++ src/main/java/com/bookstore/api/controllers/SaleController.java
@@ -3,8 +3,12 @@ package com.bookstore.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.api.models.Sale;
@@ -21,5 +25,11 @@ public class SaleController {
     @GetMapping
     public List<Sale> getSales(){
         return saleRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Sale createSale(@RequestBody Sale sale){
+        return saleRepository.save(sale);
     }
 }
